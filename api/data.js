7857e45a807e370b4888a1e60d5c63f19dd3690b
SrinjaDhar <srinja.006@gmail.com>
2021-07-05 14:21:37 +0530

    added

---
+++ api/data.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 01,
+    name: 'TAGG VERVE',
+    imageUrl:"https://cdn.taggdigital.com/webroot/img/756191611526140.png",
+    available_quantity: 5,
+    price: 4199,
+    description: 'P68 rating makes it safe from not just dust and splashes of water but even during rain pours. Wear it anywhere and everywhere! Flaunt the best iOS and android smart watches to the world and say, “watch me”!'
+  },
+
+  {
+    id: 02,
+    name: 'Microsoft Surface Laptop 3 128GB i5 8GB Platinum',
+    imageUrl:"https://cdn.alzashop.com/ImgW.ashx?fd=f16&cd=MCS252c0a0",
+    available_quantity: 7,
+    price: 40050,
+    description: 'Laptop - Intel Core i5 1035G7 Ice Lake, touchscreen 13.5" 2256 × 1504, RAM 8GB LPDDR4X, Intel Iris Plus Graphics, SSD 128GB, webcam, USB-C, Windows 10 Home'
+  },
+
+  {
+    id: 03,
+    name: 'Galaxy S21 5G',
+    imageUrl:"https://images.samsung.com/is/image/samsung/p6pim/in/galaxy-s21/gallery/in-galaxy-s21-5g-g991-371114-371114-sm-g991bzvdinu-368339376",
+    available_quantity: 0,
+    price: 60000,
+    description: 'Get amazingly clear, high-quality photos with the large 64MP sensor.* Even in dark situations, it takes in plenty of light to get the rich image quality. It’s all in the details.',
+  },
+
+  {
+    id: 04,
+    name: 'Samsung 43” FHD Flat TV N5000 Series 5',
+    imageUrl:"https://images.samsung.com/is/image/samsung/levant-hdtv-n4000-global-ua43n5000arxtw-frontblack-113496207?$720_576_PNG$",
+    available_quantity: 4,
+    price: 15000,
+    description: 'Vibrant colours with true details. Clean view with wide colour enhancer',
+  },
+];
+
+
+
+const users = [
+    {
+      'name': 'user',
+      'password': 'qwerty'
+    },
+    {
+      'name': 'example',
+      'password': 'qwerty'
+    }
+];
+
+module.exports = { 'products': products, users: users }
